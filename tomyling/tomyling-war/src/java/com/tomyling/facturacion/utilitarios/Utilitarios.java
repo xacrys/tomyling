583138ad89159de4a13d14ac310a1b29a0d74c25
Xacrys10 <Xacrys10@DESKTOP-0C76TBU>
2017-05-08 19:19:05 -0500

    generar Excel

--- tomyling/tomyling-war/src/java/com/tomyling/facturacion/utilitarios/Utilitarios.java
+++ tomyling/tomyling-war/src/java/com/tomyling/facturacion/utilitarios/Utilitarios.java
@@ -70,6 +70,8 @@ public class Utilitarios implements Serializable {
         XSSFWorkbook libro = new XSSFWorkbook();
         XSSFSheet hoja = libro.createSheet();
         XSSFRow fila1 = hoja.createRow(0);
+        XSSFCellStyle titulo = titulo(libro, "CENTER");
+        XSSFCellStyle titulo2 = titulo(libro, "LEFT");
         XSSFCellStyle styleCuerpo = stiloCuerpo1(libro);
         hoja.addMergedRegion(new CellRangeAddress(0, 0, 0, 7));
         crearCelda(fila1, 0, "Sistema de generacion de Facturas", styleCuerpo);
@@ -92,53 +94,53 @@ public class Utilitarios implements Serializable {
         hoja.addMergedRegion(new CellRangeAddress(1, 1, 0, 7));
         XSSFRow fila3 = hoja.createRow(2);
         hoja.addMergedRegion(new CellRangeAddress(2, 2, 4, 5));
-        crearCelda(fila3, 4, "RUC", styleCuerpo);
+        crearCelda(fila3, 4, "RUC", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(2, 2, 6, 7));
         crearCelda(fila3, 6, facCompleta.getFactura().getCedRuc(), styleCuerpo);
         XSSFRow fila4 = hoja.createRow(3);
         hoja.addMergedRegion(new CellRangeAddress(3, 3, 4, 7));
-        crearCelda(fila4, 4, "Factura", styleCuerpo);
+        crearCelda(fila4, 4, "Factura", titulo2);
         XSSFRow fila5 = hoja.createRow(4);
         hoja.addMergedRegion(new CellRangeAddress(4, 4, 4, 7));
         crearCelda(fila5, 4, facCompleta.getFactura().getClaveAcceso(), styleCuerpo);
         XSSFRow fila6 = hoja.createRow(5);
         hoja.addMergedRegion(new CellRangeAddress(5, 5, 4, 7));
-        crearCelda(fila6, 4, "Clave de Acceso", styleCuerpo);
+        crearCelda(fila6, 4, "Clave de Acceso", titulo2);
         XSSFRow fila7 = hoja.createRow(6);
         hoja.addMergedRegion(new CellRangeAddress(6, 6, 4, 7));
         crearCelda(fila7, 4, facCompleta.getFactura().getClaveAcceso(), styleCuerpo);
         XSSFRow fila8 = hoja.createRow(7);
         hoja.addMergedRegion(new CellRangeAddress(7, 7, 0, 1));
-        crearCelda(fila8, 0, "Direccion Matriz", styleCuerpo);
+        crearCelda(fila8, 0, "Direccion Matriz", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(7, 7, 2, 3));
         crearCelda(fila8, 2, facCompleta.getFactura().getDirMatriz(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(7, 7, 4, 5));
-        crearCelda(fila8, 4, "Fecha de Emision", styleCuerpo);
+        crearCelda(fila8, 4, "Fecha de Emision", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(7, 7, 6, 7));
         crearCelda(fila8, 6, facCompleta.getFactura().getFechaEmision().toString(), styleCuerpo);
         XSSFRow fila9 = hoja.createRow(8);
         hoja.addMergedRegion(new CellRangeAddress(8, 8, 0, 1));
-        crearCelda(fila9, 0, "Direccion Sucursal", styleCuerpo);
+        crearCelda(fila9, 0, "Direccion Sucursal", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(8, 8, 2, 3));
         crearCelda(fila9, 2, facCompleta.getFactura().getDirEstablecimiento(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(8, 8, 4, 5));
-        crearCelda(fila9, 4, "Ambiente", styleCuerpo);
+        crearCelda(fila9, 4, "Ambiente", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(8, 8, 6, 7));
         crearCelda(fila9, 6, facCompleta.getDescripcionAmbiente(), styleCuerpo);
         XSSFRow fila10 = hoja.createRow(9);
         hoja.addMergedRegion(new CellRangeAddress(9, 9, 0, 1));
-        crearCelda(fila10, 0, "Contribuyente Especial No.", styleCuerpo);
+        crearCelda(fila10, 0, "Contribuyente Especial No.", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(9, 9, 2, 3));
         crearCelda(fila10, 2, facCompleta.getFactura().getContibuyenteEspecial(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(9, 9, 4, 5));
-        crearCelda(fila10, 4, "Emision", styleCuerpo);
+        crearCelda(fila10, 4, "Emision", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(9, 9, 6, 7));
         crearCelda(fila10, 6, facCompleta.getDescripcionTipoEmision(), styleCuerpo);
         XSSFRow fila11 = hoja.createRow(10);
         hoja.addMergedRegion(new CellRangeAddress(10, 10, 0, 3));
-        crearCelda(fila11, 0, "Obligado a llevar Contabilidad", styleCuerpo);
+        crearCelda(fila11, 0, "Obligado a llevar Contabilidad", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(10, 10, 4, 7));
-        crearCelda(fila11, 4, "Clave de Acceso", styleCuerpo);
+        crearCelda(fila11, 4, "Clave de Acceso", titulo2);
         XSSFRow fila12 = hoja.createRow(11);
         hoja.addMergedRegion(new CellRangeAddress(11, 11, 0, 3));
         crearCelda(fila12, 0, facCompleta.getFactura().getObligadoContabilidad() ? "SI" : "NO", styleCuerpo);
@@ -146,11 +148,11 @@ public class Utilitarios implements Serializable {
         crearCelda(fila12, 4, facCompleta.getFactura().getClaveAcceso(), styleCuerpo);
         XSSFRow fila13 = hoja.createRow(12);
         hoja.addMergedRegion(new CellRangeAddress(12, 12, 0, 1));
-        crearCelda(fila13, 0, "Cliente", styleCuerpo);
+        crearCelda(fila13, 0, "Cliente", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(12, 12, 2, 3));
         crearCelda(fila13, 2, facCompleta.getFactura().getRazonSocial(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(12, 12, 4, 5));
-        crearCelda(fila13, 4, "Identificacion", styleCuerpo);
+        crearCelda(fila13, 4, "Identificacion", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(12, 12, 6, 7));
         crearCelda(fila13, 6, facCompleta.getFactura().getRuc(), styleCuerpo);
         XSSFRow fila14 = hoja.createRow(13);
@@ -158,27 +160,116 @@ public class Utilitarios implements Serializable {
 
         XSSFRow fila15 = hoja.createRow(14);
         hoja.addMergedRegion(new CellRangeAddress(14, 14, 0, 1));
-        crearCelda(fila15, 0, "Cliente", styleCuerpo);
+        crearCelda(fila15, 0, "Cliente", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(14, 14, 2, 3));
         crearCelda(fila15, 2, facCompleta.getFactura().getRazonSocial(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(14, 14, 4, 5));
-        crearCelda(fila15, 4, "Identificacion", styleCuerpo);
+        crearCelda(fila15, 4, "Identificacion", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(14, 14, 6, 7));
         crearCelda(fila15, 6, facCompleta.getFactura().getRuc(), styleCuerpo);
 
         XSSFRow fila16 = hoja.createRow(15);
         hoja.addMergedRegion(new CellRangeAddress(15, 15, 0, 1));
-        crearCelda(fila16, 0, "Fecha de Emision", styleCuerpo);
+        crearCelda(fila16, 0, "Fecha de Emision", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(15, 15, 2, 3));
         crearCelda(fila16, 2, facCompleta.getFactura().getFechaEmision().toString(), styleCuerpo);
         hoja.addMergedRegion(new CellRangeAddress(15, 15, 4, 5));
-        crearCelda(fila16, 4, "Guia de Remision", styleCuerpo);
+        crearCelda(fila16, 4, "Guia de Remision", titulo2);
         hoja.addMergedRegion(new CellRangeAddress(15, 15, 6, 7));
-        crearCelda(fila16, 6, facCompleta.getFactura().getGuiaRemision()==null?"":facCompleta.getFactura().getGuiaRemision(), styleCuerpo);
+        crearCelda(fila16, 6, facCompleta.getFactura().getGuiaRemision() == null ? "" : facCompleta.getFactura().getGuiaRemision(), styleCuerpo);
         XSSFRow fila17 = hoja.createRow(16);
         hoja.addMergedRegion(new CellRangeAddress(16, 16, 0, 7));
+        agregarTabla(hoja, facCompleta.getListaDetalle(), styleCuerpo, titulo2);
+        Integer ultimafila = hoja.getLastRowNum() + 1;
+        XSSFRow filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 7));
 
-        agregarTabla(hoja, facCompleta.getListaDetalle(), styleCuerpo);
+        //SECCION SUBTOTAL Y TOTAL
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "Subtotal 14%", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getFactura().getTotalSinimpuesto().toString(), styleCuerpo);
+        //SUBTOTAL
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "Subtotal 0%", titulo2);
+        crearCelda(filaNueva, 7, "0.00", styleCuerpo);
+        //SUBTOTAL NO OBJETO DE IVA
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "Subtotal No Objeto de IVA", titulo2);
+        crearCelda(filaNueva, 7, "0.00", styleCuerpo);
+        //SUBTOTAL NO EXCENTO DE IVA
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 1));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 2, 3));
+        crearCelda(filaNueva, 0, "Direccion", titulo2);
+        crearCelda(filaNueva, 2, facCompleta.getFactura().getDireccionAdicional() == null ? "" : facCompleta.getFactura().getDireccionAdicional(), styleCuerpo);
+        crearCelda(filaNueva, 5, "Subtotal Excento de IVA", titulo2);
+        crearCelda(filaNueva, 7, "0.00", styleCuerpo);
+        //SUBTOTAL SIN IMPUESTOS
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 1));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 2, 3));
+        crearCelda(filaNueva, 0, "Telefono", titulo2);
+        crearCelda(filaNueva, 2, facCompleta.getFactura().getTelefonoAdicional() == null ? "" : facCompleta.getFactura().getTelefonoAdicional(), styleCuerpo);
+        crearCelda(filaNueva, 5, "Subtotal Sin Impuestos", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getFactura().getTotalSinimpuesto().toString(), styleCuerpo);
+        //Total Descuento
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 1));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 2, 3));
+        crearCelda(filaNueva, 0, "Vendedor", titulo2);
+        crearCelda(filaNueva, 2, facCompleta.getFactura().getVendedorAdicional() == null ? "" : facCompleta.getFactura().getVendedorAdicional(), styleCuerpo);
+        crearCelda(filaNueva, 5, "Total Descuento", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getFactura().getTotalDescuento().toString(), styleCuerpo);
+        //ICE
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 1));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 2, 3));
+        crearCelda(filaNueva, 0, "Cuota", titulo2);
+        crearCelda(filaNueva, 2, facCompleta.getFactura().getCuotaAdicional() == null ? "" : facCompleta.getFactura().getCuotaAdicional(), styleCuerpo);
+        crearCelda(filaNueva, 5, "ICE", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getValorIce().toString(), styleCuerpo);
+        //IVA
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 0, 1));
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 2, 3));
+        crearCelda(filaNueva, 0, "Forma de Pago", titulo2);
+        crearCelda(filaNueva, 2, facCompleta.getFactura().getFormaPagoAdicional() == null ? "" : facCompleta.getFactura().getFormaPagoAdicional(), styleCuerpo);
+        crearCelda(filaNueva, 5, "IVA 14%", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getValor14().toString(), styleCuerpo);
+        //IRBPNR
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "IRBPNR", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getValorIrbf().toString(), styleCuerpo);
+        //Propinas
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "Propina", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getFactura().getPropina().toString(), styleCuerpo);
+        //Valor Total
+        ultimafila = hoja.getLastRowNum() + 1;
+        filaNueva = hoja.createRow(ultimafila);
+        hoja.addMergedRegion(new CellRangeAddress(ultimafila, ultimafila, 5, 6));
+        crearCelda(filaNueva, 5, "Subtotal Sin Impuestos", titulo2);
+        crearCelda(filaNueva, 7, facCompleta.getFactura().getImporteTotal().toString(), styleCuerpo);
 
         String nombreExcel = "Factura.xlsx";
         try (FileOutputStream archivoSalida = new FileOutputStream(nombreExcel)) {
@@ -238,6 +329,25 @@ public class Utilitarios implements Serializable {
         return celda;
     }
 
+    public XSSFCellStyle titulo(XSSFWorkbook libro, String alineamiento) {
+// Se crea el estilo para el cuerpo
+        XSSFCellStyle styleCuerpo = libro.createCellStyle();
+        if (alineamiento.equals("LEFT")) {
+            styleCuerpo.setAlignment(HorizontalAlignment.LEFT);
+        } else {
+            styleCuerpo.setAlignment(HorizontalAlignment.CENTER);
+        }
+
+        styleCuerpo.setVerticalAlignment(VerticalAlignment.CENTER);
+        XSSFFont font = libro.createFont();
+        font.setFontName("Verdana");
+        font.setBold(true);
+        font.setItalic(true);
+        styleCuerpo.setFont(font);
+        styleCuerpo.setWrapText(true);
+        return styleCuerpo;
+    }
+
     public XSSFCellStyle stiloCuerpo1(XSSFWorkbook libro) {
 // Se crea el estilo para el cuerpo
         XSSFCellStyle styleCuerpo = libro.createCellStyle();
@@ -245,38 +355,35 @@ public class Utilitarios implements Serializable {
         styleCuerpo.setVerticalAlignment(VerticalAlignment.CENTER);
         XSSFFont font = libro.createFont();
         font.setFontName("Verdana");
-        font.setBold(true);
-        font.setItalic(true);
-        styleCuerpo.setBorderBottom(BorderStyle.THIN);
-        styleCuerpo.setBorderTop(BorderStyle.THIN);
-        styleCuerpo.setBorderLeft(BorderStyle.THIN);
-        styleCuerpo.setBorderRight(BorderStyle.THIN);
+        font.setBold(false);
+        font.setItalic(false);
+        font.setFamily(10);
         styleCuerpo.setFont(font);
         styleCuerpo.setWrapText(true);
         return styleCuerpo;
     }
 
-    public void agregarTabla(XSSFSheet hoja, List<DetalleFactura> lista, XSSFCellStyle estilo) {
+    public void agregarTabla(XSSFSheet hoja, List<DetalleFactura> lista, XSSFCellStyle estilo, XSSFCellStyle estilo2) {
         XSSFRow fila = hoja.createRow(hoja.getLastRowNum() + 1);
-        crearCelda(fila, 0, "Item", estilo);
-        crearCelda(fila, 1, "Codigo Principal", estilo);
-        crearCelda(fila, 2, "Codigo Auxiliar", estilo);
-        crearCelda(fila, 3, "Descripcion", estilo);
-        crearCelda(fila, 4, "Cantidad", estilo);
-        crearCelda(fila, 5, "Precio", estilo);
-        crearCelda(fila, 6, "Descuento", estilo);
-        crearCelda(fila, 7, "Precio Total", estilo);
+        crearCelda(fila, 0, "Item", estilo2);
+        crearCelda(fila, 1, "Codigo Principal", estilo2);
+        crearCelda(fila, 2, "Codigo Auxiliar", estilo2);
+        crearCelda(fila, 3, "Descripcion", estilo2);
+        crearCelda(fila, 4, "Cantidad", estilo2);
+        crearCelda(fila, 5, "Precio", estilo2);
+        crearCelda(fila, 6, "Descuento", estilo2);
+        crearCelda(fila, 7, "Precio Total", estilo2);
         for (DetalleFactura df : lista) {
             XSSFRow filaNueva = hoja.createRow(hoja.getLastRowNum() + 1);
             crearCelda(filaNueva, 0, df.getIdDetalle().toString(), estilo);
             crearCelda(filaNueva, 1, df.getCodigoPrincipal(), estilo);
-            crearCelda(filaNueva, 2, df.getCodigoAuxiliar()==null?"":df.getCodigoAuxiliar(), estilo);
+            crearCelda(filaNueva, 2, df.getCodigoAuxiliar() == null ? "" : df.getCodigoAuxiliar(), estilo);
             crearCelda(filaNueva, 3, df.getDescripcion(), estilo);
             crearCelda(filaNueva, 4, df.getCantidad().toString(), estilo);
             crearCelda(filaNueva, 5, df.getPrecioUnitario().toString(), estilo);
             crearCelda(filaNueva, 6, df.getDescuento().toString(), estilo);
             crearCelda(filaNueva, 7, df.getPrecioTotalSinimpuesto().toString(), estilo);
-            
+
         }
 
     }
